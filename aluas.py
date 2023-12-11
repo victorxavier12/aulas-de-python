865c480013144b3273dfd00c94b4702e1e4875ab
Victor de Albuquerque Xavier <victorxav.01@gmail.com>
2023-12-11 15:42:28 +0000

    update

---
+++ aluas.py
@@ -0,0 +1,27 @@
+animais = {
+    "nome" : ["pepe" , "miguel" , "roberto"],
+    "especie" : ["cão" , "leão" , "jumento"],
+    "idade" : [7 , 15, 5],
+    "cor" : ["vermelho" , "listrado" , "cinza"],
+}
+
+    # nome de animais em ordem 123 V 
+nomeani_1 = animais ["nome"][0]
+nomeani_2 = animais ["nome"][1]
+nomeani_3 = animais ["nome"][2] 
+    # especie de animais em ordem 123 V
+especieani_1 = animais ["especie"][0]
+especieani_2 = animais ["especie"][1]
+especieani_3 = animais ["especie"][2] 
+    #idade de animais em oredm 123 V
+idadeani_1 = animais ["idade"][0]
+idadeani_2 = animais ["idade"][1]
+idadeani_3 = animais ["idade"][2] 
+    #cor dos animais em ordem 123 V
+corani_1 = animais ["cor"][0]
+corani_2 = animais ["cor"][1]
+corani_3 = animais ["cor"][2] 
+
+print ("o primeiro animal da miha lista tem o nome de:", nomeani_1 , "ele é de especie:", especieani_1 , "a idade dele é:", idadeani_1 , "e por outimo sua cor é", corani_1 , 
+       "tambem temos um", especieani_2 , "que sua cor é:", corani_2 , "seu nome:", nomeani_2 , " que tem", idadeani_2 , "anos de idade, e por ultimo temos o", nomeani_3 , "que é um", especieani_3 , "muito simpatico de ", idadeani_3 , "anos de idade de cor", corani_3 , "muito bonito esses são todos os animais."
+)
