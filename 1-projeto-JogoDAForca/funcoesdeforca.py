6bc714c6a83a8fc861a3bfed0f6ce001d63f21ef
Victor de Albuquerque Xavier <victorxav.01@gmail.com>
2024-01-16 23:17:14 +0000

    update

--- 1-projeto-JogoDAForca/funcoesdeforca.py
+++ 1-projeto-JogoDAForca/funcoesdeforca.py
@@ -2,4 +2,4 @@ import random
 
 def escolher_palavra(): #escolher aleatoriamento uma palavra
     palavras = ["python", "programacao", "computador", "jogo", "desenvolvimento"]
-    return random.choice(palavras)
+    return random.choice(palavras)  
