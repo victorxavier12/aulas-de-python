c4a12864e0a24f8eae42393c518364acbc1b3252
Victor de Albuquerque Xavier <victorxav.01@gmail.com>
2024-01-16 20:17:17 +0000

    update

--- 1-projeto-JogoDAForca/funcoesdeforca.py
+++ 1-projeto-JogoDAForca/funcoesdeforca.py
@@ -3,4 +3,3 @@ import random
 def escolher_palavra(): #escolher aleatoriamento uma palavra
     palavras = ["python", "programacao", "computador", "jogo", "desenvolvimento"]
     return random.choice(palavras)
-
--- 1-projeto-JogoDAForca/jogoDaForca.py
+++ 1-projeto-JogoDAForca/jogoDaForca.py
@@ -1,14 +1,11 @@
 import funcoesdeforca as fc
 
-
 while True:
     fc.escolher_palavra() # chama a função que randomisa palavras 
-
     palavra_secreta = fc.escolher_palavra() # variavel que recebe a palavra e armazena
-
     letras_usuario = []
-    chanches = 10 
-
+    letras_incorretas = []
+    chanches = 6 
     print('####  JOGO DA FORCA  ####')
     while True:
         
@@ -17,25 +14,27 @@ while True:
                 print(i, end=" ")
             else: 
                 print('_', end=' ')
-        
         print(f'voce tem {chanches} para adivinhar')
+        print("\nletras incorretas usadas: ", end="")
+        for i in letras_usuario:
+            if i.lower() not in palavra_secreta:
+                print( i , end=", ")
 
-
-        print(f"letras usadas: {letras_usuario}")
-
-        letra = input("\nEscolha uma letra para adivinhar: ")
+        letra = input("\n\nEscolha uma letra para adivinhar: ")
         letras_usuario.append(letra.lower())
-
+        if letra.lower() in palavra_secreta.lower():
+            print("\nPARABENS ACERTOU UMA DAS LETRAS")
+ 
         if letra.lower() not in palavra_secreta.lower():
             chanches -= 1
-
+            print("\n!!!!voce errou a letra!!!!")
 
         ganhou = True
         for i in palavra_secreta:
             if i.lower() not in letras_usuario:
                 ganhou = False
         
-        if chanches == 0 or ganhou==True:
+        if chanches == 0 or ganhou == True:
             break
 
     if chanches == 0:
@@ -44,16 +43,12 @@ while True:
         print(f"\n!!!!PARABENS VOCE GANHOU A PALAVRA ERA : {palavra_secreta.upper()} !!!!\n")
     while True:
         print('DEJEJA TENTAR NOVAMENTE?\n')
-        tentativa = input("(1) -  SIM / (2) - NÃO\n")
-        if tentativa == "1":
+        novojogo = input("(1) -  SIM / (2) - NÃO\n")
+        if novojogo == "1":
             break
-        elif tentativa == "2":
+        elif novojogo == "2":
             break
         else:
             print("invalida selecione uma alternativa valida\n")
-    if tentativa == "2":
+    if novojogo == "2":
         break
-
-    
-    
-    
